0591e8bb4f06c755867e8904c229ddbf066542d5
Your Name <youremail@example.com>
2025-01-19 15:24:02 -0500

    MERGE COMPLETE

--- weather_api/notebooks/weather_api.py
+++ weather_api/notebooks/weather_api.py
@@ -3,7 +3,8 @@ import pandas as pd
 import json
 import os
 '''
-Goal: to retrieve professionally collected weather data from an online source to predict coffee harvests in Minas Gerais, Brazil 
+Goal: to retrieve professionally collected weather data from an
+online source to predict coffee harvests in Minas Gerais, Brazil 
 '''
 url = "https://historical-forecast-api.open-meteo.com/v1/forecast?latitude=-21.72&longitude=-45.39&start_date=2022-01-01&end_date=2023-12-31&hourly=temperature_2m,relative_humidity_2m,precipitation,surface_pressure"
 jason_data = requests.get(url)
@@ -18,5 +19,4 @@ df = pd.DataFrame(measurements["hourly"])
 #changed the colmumn names for smoother processing
 df.columns = ["time", "temperature", "humidity", "precipitation", "pressure"]
 #change time to datetime for uniformity
-print(df)
-#df.to_csv("/Users/sa12/Documents/Repositories/WeatherDataMG/weather_api/data/csv/measurements.csv", index=False)
+df.to_csv("/Users/sa12/Documents/Repositories/WeatherDataMG/weather_api/data/csv/measurements.csv", index=False)
