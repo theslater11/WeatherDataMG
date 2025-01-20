98771fcf92fa8c50e55232899c682f2b3c945565
Your Name <youremail@example.com>
2025-01-20 16:53:22 -0500

    Finalized

--- weather_api/weather_api.py
+++ weather_api/weather_api.py
@@ -1,11 +1,11 @@
-import requests
-import pandas as pd
-import json
-import os
 '''
 Goal: to retrieve professionally collected weather data from an
 online source to predict coffee harvests in Minas Gerais, Brazil 
 '''
+import requests
+import pandas as pd
+import json
+import os
 url = "https://historical-forecast-api.open-meteo.com/v1/forecast?latitude=-21.72&longitude=-45.39&start_date=2022-01-01&end_date=2023-12-31&hourly=temperature_2m,relative_humidity_2m,precipitation,surface_pressure"
 jason_data = requests.get(url)
 #pull data from weather data website
